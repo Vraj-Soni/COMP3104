8327c4f242f15277f707e01571da21c2dce01ef4
Vraj-Soni <sonivraj848@gmail.com>
2021-10-28 12:35:45 -0400

    updated files

---
+++ build/precache-manifest.9c02ea81f6ddb7ac89492736115b16e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef213a503e0588bdbd90",
+    "url": "/Vraj-Soni/COMP3104/static/css/main.e7423cb0.chunk.css"
+  },
+  {
+    "revision": "ef213a503e0588bdbd90",
+    "url": "/Vraj-Soni/COMP3104/static/js/main.ef213a50.chunk.js"
+  },
+  {
+    "revision": "f9edad08bc0b847a096f",
+    "url": "/Vraj-Soni/COMP3104/static/js/runtime~main.f9edad08.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Vraj-Soni/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "7d02362002ad49435df1346072acb2b3",
+    "url": "/Vraj-Soni/COMP3104/index.html"
+  }
+];
